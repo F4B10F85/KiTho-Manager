912a366226b71208106e17fb3168fa9666b606d0
Fabio <xk73mh31@gmail.com>
2026-07-20 23:27:16 +0200

    Create application entry point

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| KiTho Manager
+|--------------------------------------------------------------------------
+| File principale dell'applicazione.
+| Da qui partirà l'intero gestionale.
+|--------------------------------------------------------------------------
+*/
+
+document.addEventListener("DOMContentLoaded", initializeApplication);
+
+/**
+ * Avvia KiTho Manager.
+ */
+function initializeApplication() {
+
+    console.clear();
+
+    console.log("========================================");
+    console.log("        KiTho Manager v0.1.0");
+    console.log("========================================");
+
+    console.log("Application started.");
+
+}
